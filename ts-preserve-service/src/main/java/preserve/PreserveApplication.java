0c498e1a0006d44794f670f247d405d226887b4b
Ji Chao <chaojifudan@outlook.com>
2018-01-10 15:42:13 +0800

    fault reproduce

--- ts-preserve-service/src/main/java/preserve/PreserveApplication.java
+++ ts-preserve-service/src/main/java/preserve/PreserveApplication.java
@@ -1,5 +1,7 @@
 package preserve;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,12 +10,16 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
+import preserve.domain.StatusBean;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableAsync
 @IntegrationComponentScan
-public class PreserveApplication  {
+public class PreserveApplication implements CommandLineRunner {
+
+    @Autowired
+    private StatusBean statusBean;
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PreserveApplication.class, args);
@@ -22,6 +28,17 @@ public class PreserveApplication  {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Bean
+    public StatusBean statusBean() {
+        return new StatusBean();
+    }
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        // TODO Auto-generated method stub
+        statusBean.chartMsgs.clear();
     }
 
 }
