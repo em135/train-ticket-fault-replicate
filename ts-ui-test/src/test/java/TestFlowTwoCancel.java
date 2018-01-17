109f6420c25d0b88495ce672038528a22a9c2c00
Ji Chao <chaojifudan@outlook.com>
2018-01-17 11:10:56 +0800

    test flow

--- ts-ui-test/src/test/java/TestFlowTwoCancel.java
+++ ts-ui-test/src/test/java/TestFlowTwoCancel.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -9,7 +8,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class TestFlowTwoPay {
+public class TestFlowTwoCancel {
     private WebDriver driver;
     private String baseUrl;
     private List<WebElement> myOrdersList;
@@ -25,7 +24,7 @@ public class TestFlowTwoPay {
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "D:/Program/chromedriver_win32/chromedriver.exe");
         driver = new ChromeDriver();
-        baseUrl = "http://10.141.212.24/";
+        baseUrl = "http://10.141.212.21/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     @Test
@@ -43,31 +42,37 @@ public class TestFlowTwoPay {
 
         //get login status
         String statusLogin = driver.findElement(By.id("flow_preserve_login_msg")).getText();
-        if("".equals(statusLogin))
+        if("".equals(statusLogin)) {
             System.out.println("Failed to Login! Status is Null!");
-        else if(statusLogin.startsWith("Success"))
-            System.out.println("Success to Login! Status:"+statusLogin);
-        else
-            System.out.println("Failed to Login! Status:"+statusLogin);
+        } else if(statusLogin.startsWith("Success")) {
+            System.out.println("Success to Login! Status:" + statusLogin);
+        } else {
+            System.out.println("Failed to Login! Status:" + statusLogin);
+        }
         Assert.assertEquals(statusLogin.startsWith("Success"),true);
+
+        Thread.sleep(1000);
+        String js = "document.getElementById('flow_two_page').scrollIntoView(false)";
+        ((JavascriptExecutor)driver).executeScript(js);
+        Thread.sleep(1000);
+
         driver.findElement(By.id("flow_two_page")).click();
     }
     @Test (dependsOnMethods = {"testLogin"})
     public void testViewOrders() throws Exception{
-        driver.findElement(By.id("flow_two_page")).click();
         driver.findElement(By.id("refresh_my_order_list_button")).click();
         Thread.sleep(1000);
         //gain my oeders
         myOrdersList = driver.findElements(By.xpath("//div[@id='my_orders_result']/div"));
         if (myOrdersList.size() > 0) {
             System.out.printf("Success to show my orders list，the list size is:%d%n",myOrdersList.size());
-        }
-        else
+        } else {
             System.out.println("Failed to show my orders list，the list size is 0 or No orders in this user!");
+        }
         Assert.assertEquals(myOrdersList.size() > 0,true);
     }
     @Test (dependsOnMethods = {"testViewOrders"})
-    public void testPayOrder() throws Exception{
+    public void testClickOrderCancel() throws Exception{
         System.out.printf("The orders list size is:%d%n",myOrdersList.size());
         String statusOrder  = "";
         int i;
@@ -76,32 +81,30 @@ public class TestFlowTwoPay {
         //while(!(statusOrder.startsWith("Not")) && i < myOrdersList.size()) {
             //statusOrder = myOrdersList.get(i).findElement(By.xpath("/div[2]/div/div/form/div[7]/div/label[2]")).getText();
             statusOrder = myOrdersList.get(i).findElement(By.xpath("div[2]//form[@role='form']/div[7]/div/label[2]")).getText();
-            if(statusOrder.startsWith("Not"))
+            if(statusOrder.startsWith("Paid"))
                 break;
         }
         if(i == myOrdersList.size() || i > myOrdersList.size())
             System.out.printf("Failed,there is no not paid order!");
         Assert.assertEquals(i < myOrdersList.size(),true);
 
-        myOrdersList.get(i).findElement(By.xpath("div[2]//form[@role='form']/div[7]/div/button")).click();
         Thread.sleep(1000);
-        String inputNotPaidOrderId = driver.findElement(By.id("pay_for_not_paid_orderId")).getAttribute("value");
-        String inputNotPaidPrice = driver.findElement(By.id("pay_for_not_paid_price")).getAttribute("value");
-        String inputNotPaidTripId = driver.findElement(By.id("pay_for_not_paid_tripId")).getAttribute("value");
-        boolean bNotPaidOrderId = !"".equals(inputNotPaidOrderId);
-        boolean bNotPaidPrice = !"".equals(inputNotPaidPrice);
-        boolean bNotPaidTripId = !"".equals(inputNotPaidTripId);
-        boolean bNotPaidStatus = bNotPaidOrderId && bNotPaidPrice && bNotPaidTripId;
-        if(bNotPaidStatus == false)
-            System.out.println("Step-Pay for Your Order,The input is null!!");
-        Assert.assertEquals(bNotPaidStatus,true);
+        String js = "document.getElementsByClassName('ticket_cancel_btn')[0].scrollIntoView(false)";
+        ((JavascriptExecutor)driver).executeScript(js);
+        Thread.sleep(1000);
 
-        driver.findElement(By.id("pay_for_not_paid_pay_button")).click();
-        Thread.sleep(1000);
+        myOrdersList.get(i).findElement(
+                By.xpath("div[2]//form[@role='form']/div[12]/div/button[@class='ticket_cancel_btn btn btn-primary']")
+        ).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("ticket_cancel_panel_confirm")).click();
+
+        Thread.sleep(10000);
 
         Alert javascriptConfirm = driver.switchTo().alert();
         String statusAlert = driver.switchTo().alert().getText();
-        System.out.println("The Alert information of Payment："+statusAlert);
+        System.out.println("The Alert information of Cancel Ticket："+statusAlert);
         Assert.assertEquals(statusAlert.startsWith("Success"),true);
         javascriptConfirm.accept();
     }
