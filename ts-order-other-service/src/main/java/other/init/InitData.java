adb81f587ffa0141a75c035c1bd4dea73d1fc0e8
Ji Chao <chaojifudan@outlook.com>
2018-01-24 11:07:37 +0800

    add one order

--- ts-order-other-service/src/main/java/other/init/InitData.java
+++ ts-order-other-service/src/main/java/other/init/InitData.java
@@ -1,9 +1,13 @@
 package other.init;
 
+import other.domain.Order;
+import other.domain.OrderStatus;
 import other.service.OrderOtherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import java.util.Date;
+import java.util.UUID;
 
 @Component
 public class InitData implements CommandLineRunner {
@@ -11,7 +15,24 @@ public class InitData implements CommandLineRunner {
     OrderOtherService service;
 
     public void run(String... args)throws Exception{
-
+        Order order = new Order();
+        order.setId(UUID.fromString("5ad7750b-a68b-49c0-a8c0-32776b067703"));
+        order.setBoughtDate(new Date());
+        order.setTravelDate(new Date("Sun Jul 29 00:00:00 GMT+0800 2018"));
+        order.setTravelTime(new Date("Mon May 04 09:02:00 GMT+0800 2013"));
+        order.setAccountId(UUID.fromString("4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f"));
+        order.setContactsName("Contacts_One");
+        order.setDocumentType(1);
+        order.setContactsDocumentNumber("DocumentNumber_One");
+        order.setTrainNumber("Z1234");
+        order.setCoachNumber(5);
+        order.setSeatClass(2);
+        order.setSeatNumber("1");
+        order.setFrom("shanghai");
+        order.setTo("nanjing");
+        order.setStatus(OrderStatus.PAID.getCode());
+        order.setPrice("100.0");
+        service.initOrder(order);
     }
 
 }
