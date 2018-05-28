ef88e0aa3ca0ce64352a7b0660cb51f5d5fc7333
Ji Chao <chaojifudan@outlook.com>
2018-05-28 15:32:36 +0800

    blank line

--- ts-order-other-service/src/main/java/other/OrderOtherApplication.java
+++ ts-order-other-service/src/main/java/other/OrderOtherApplication.java
@@ -15,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @IntegrationComponentScan
 public class OrderOtherApplication {
 
+    
     public static void main(String[] args) throws Exception {
         SpringApplication.run(OrderOtherApplication.class, args);
     }
