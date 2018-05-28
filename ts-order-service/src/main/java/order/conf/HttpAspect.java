0082303569a80d4a6d4c9a8300c438e7cfd922cd
Ji Chao <chaojifudan@outlook.com>
2018-05-28 15:29:28 +0800

    tet

---
+++ ts-order-service/src/main/java/order/conf/HttpAspect.java
@@ -0,0 +1,57 @@
+package order.conf;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class HttpAspect {
+
+    private final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HttpAspect.class);
+
+    @Pointcut("execution(public * order.controller.OrderController.*(..))")
+    public void webLog(){}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint){
+
+        logger.info("doBefore==================================");
+
+        //记录http请求
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        //从request中获取http请求的url/请求的方法类型／响应该http请求的类方法／IP地址／请求中的参数
+        //url
+        logger.info("url={}",request.getRequestURI());
+
+        //method
+        logger.info("method={}",request.getMethod());
+
+        //ip
+        logger.info("ip={}",request.getRemoteAddr());
+
+        //类方法
+        logger.info("class_method={}",joinPoint.getSignature().getDeclaringTypeName()+
+                "."+joinPoint.getSignature().getName());
+
+        //参数
+
+        logger.info("args={}",joinPoint.getArgs());
+    }
+
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.info("RESPONSE : " + ret); 
+    }
+}
