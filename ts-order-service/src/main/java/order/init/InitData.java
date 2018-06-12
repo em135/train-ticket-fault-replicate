0800ddf6d39f2b4361d5cc505568f0265acfdab5
Ji Chao <chaojifudan@outlook.com>
2018-06-12 15:09:46 +0800

    ..

--- ts-order-service/src/main/java/order/init/InitData.java
+++ ts-order-service/src/main/java/order/init/InitData.java
@@ -31,7 +31,7 @@ public class InitData implements CommandLineRunner {
         order.setSeatNumber("FirstClass-30");
         order.setFrom("nanjing");
         order.setTo("shanghaihongqiao");
-        order.setStatus(0);
+        order.setStatus(6);
         order.setPrice("100.0");
         service.initOrder(order);
 
@@ -51,7 +51,7 @@ public class InitData implements CommandLineRunner {
         orderTwo.setSeatNumber("FirstClass-30");
         orderTwo.setFrom("shanghai");
         orderTwo.setTo("beijing");
-        orderTwo.setStatus(0);
+        orderTwo.setStatus(6);
         orderTwo.setPrice("100.0");
         service.initOrder(orderTwo);
 
@@ -70,7 +70,7 @@ public class InitData implements CommandLineRunner {
         orderThree.setSeatNumber("FirstClass-30");
         orderThree.setFrom("shanghai");
         orderThree.setTo("beijing");
-        orderThree.setStatus(0);
+        orderThree.setStatus(6);
         orderThree.setPrice("100.0");
         service.initOrder(orderThree);
     }
