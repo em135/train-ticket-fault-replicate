6bebbdb0bbb0270c5ecd18d199b4a958e4528791
Ji Chao <chaojifudan@outlook.com>
2018-06-13 13:55:25 +0800

    fix

--- ts-consign-price-service/src/main/java/consignprice/service/ConsignPriceServiceImpl.java
+++ ts-consign-price-service/src/main/java/consignprice/service/ConsignPriceServiceImpl.java
@@ -43,7 +43,10 @@ public class ConsignPriceServiceImpl implements ConsignPriceService {
         String ret;
         switch (format){
             case ConstFormat.FRENCH_FORMAT :
-                ret = "₣" + temp.replace(","," ");;
+                temp = temp.replace("."," ");
+                temp = temp.replace(",",".");
+                temp = temp.replace(" ",",");
+                ret = "₣" + temp;
                 break;
             case ConstFormat.GERMAN_FORMAT:
                 temp = temp.replace("."," ");
