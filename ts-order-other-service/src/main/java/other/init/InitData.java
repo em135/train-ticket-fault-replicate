7b14dabffb59ca832ae5acb80020cb770be3b8f7
Ji Chao <chaojifudan@outlook.com>
2018-06-13 09:26:30 +0800

    add desc

--- ts-order-other-service/src/main/java/other/init/InitData.java
+++ ts-order-other-service/src/main/java/other/init/InitData.java
@@ -34,6 +34,26 @@ public class InitData implements CommandLineRunner {
         order.setPrice("100.0");
         service.initOrder(order);
 
+
+        order.setId(UUID.fromString("5ad7750b-a68b-44c0-a9c0-32776b127703"));
+        order.setBoughtDate(new Date());
+        order.setTravelDate(new Date("Sat Jul 29 00:00:00 GMT+0800 2018"));
+        order.setTravelTime(new Date("Mon May 04 09:02:00 GMT+0800 2013"));
+        order.setAccountId(UUID.fromString("4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f"));
+        order.setContactsName("Contacts_One");
+        order.setDocumentType(1);
+        order.setContactsDocumentNumber("DocumentNumber_One");
+        order.setTrainNumber("Z1234");
+        order.setCoachNumber(5);
+        order.setSeatClass(2);
+        order.setSeatNumber("30");
+        order.setFrom("shanghai");
+        order.setTo("nanjing");
+        order.setStatus(1);
+        order.setPrice("100.0");
+        service.initOrder(order);
+
+
     }
 
 }
