d3474ce0ff632743ac5163d8bafde29fb6f2dd98
Ji Chao <chaojifudan@outlook.com>
2018-06-14 11:02:32 +0800

    fix

--- ts-voucher-service/server.py
+++ ts-voucher-service/server.py
@@ -74,7 +74,6 @@ class GetVoucherHandler(tornado.web.RequestHandler):
         #查询语句
         sql = 'SELECT * FROM voucher where order_id = %s'
         try:
-            cur.callproc('testSleep')
             cur.execute(sql,(orderId))
             voucher = cur.fetchone()
             conn.commit()
@@ -130,15 +129,6 @@ def initDatabase():
         pass
 
     # create procedure
-    sql = "CREATE PROCEDURE testSleep()\
-        BEGIN\
-            select sleep(10);\
-        END"
-    try:
-        cur.execute(sql)
-        connect.commit()
-    finally:
-        pass
 
     #Create the table
     sql = """
